28ea5aed06de1e3a69f6c2dbbd7219d39f876a25
Onkar Patil <onkar.patil@krita.ai>
2023-12-29 20:05:44 +0530

    Onkar : testing deployment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import Container from "./Components/Container";
 import Container2 from "./Components2/Components/Container"; // Adjust import path as needed
 import "./App.css";
 
-
 function App() {
   return (
     <Router basename="/Krita-Calculator">
       <Routes>
-        <Route path="/Krita-Calculator" element={<Container />} />
+        <Route path="/" element={<Container />} />
         <Route path="/Krita-Calculator2" element={<Container2 />} />
       </Routes>
     </Router>
